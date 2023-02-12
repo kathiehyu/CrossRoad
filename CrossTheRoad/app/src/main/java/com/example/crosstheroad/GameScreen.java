47eb126b8e0c2ae6f0e5f5bd2b47554ce84a1547
Kathie Huynh <kathiehyu@gmail.com>
2023-02-12 17:16:21 -0500

    display player name on game start screen

--- CrossTheRoad/app/src/main/java/com/example/crosstheroad/GameScreen.java
+++ CrossTheRoad/app/src/main/java/com/example/crosstheroad/GameScreen.java
@@ -3,6 +3,7 @@ package com.example.crosstheroad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 /**
  * This class will display the first map the player will see. (activity_game_screen.xml)
@@ -14,5 +15,7 @@ public class GameScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+        TextView displayName = findViewById(R.id.playerName);
+        displayName.setText(Configuration.editName);
     }
 }
