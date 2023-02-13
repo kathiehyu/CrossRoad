64e623da82b8a6bd76c8437fdf0731bed9c3c2a0
Kathie Huynh <kathiehyu@gmail.com>
2023-02-13 10:15:32 -0500

    Change background of selected character

--- CrossTheRoad/app/src/main/java/com/example/crosstheroad/Configuration.java
+++ CrossTheRoad/app/src/main/java/com/example/crosstheroad/Configuration.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -37,6 +38,18 @@ public class Configuration extends AppCompatActivity {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
                 charButton = findViewById(charac.getCheckedRadioButtonId());
+                charButton.setBackgroundColor(Color.parseColor("lightgrey"));
+
+                if (Configuration.charButton.getId() == R.id.character1) {
+                    findViewById(R.id.character2).setBackgroundColor(0x0);
+                    findViewById(R.id.character3).setBackgroundColor(0x0);
+                } else if (Configuration.charButton.getId() == R.id.character2) {
+                    findViewById(R.id.character1).setBackgroundColor(0x0);
+                    findViewById(R.id.character3).setBackgroundColor(0x0);
+                } else {
+                    findViewById(R.id.character2).setBackgroundColor(0x0);
+                    findViewById(R.id.character1).setBackgroundColor(0x0);
+                }
             }
         });
 
