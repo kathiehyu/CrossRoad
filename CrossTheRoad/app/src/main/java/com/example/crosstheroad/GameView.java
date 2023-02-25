cd7697a2c7f52fbe6cad32531c9238c8fa2809cb
kathiehyu <kathiehyu@gmail.com>
2023-02-24 23:44:09 -0500

    fix crash when clicking start game

--- CrossTheRoad/app/src/main/java/com/example/crosstheroad/GameView.java
+++ CrossTheRoad/app/src/main/java/com/example/crosstheroad/GameView.java
@@ -104,7 +104,7 @@ public class GameView extends SurfaceView implements Runnable {
         }
     }
 
-    //    @Override
+//        @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_game_view);
