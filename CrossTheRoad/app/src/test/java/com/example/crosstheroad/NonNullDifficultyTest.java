492c8216e11c9747184137ee99e7fe79ed62b282
Shriya Jitta <sjitta6@gatech.edu>
2023-02-26 18:37:29 -0500

    unit test for when non-null value is passed into difficulty level in config class

---
+++ CrossTheRoad/app/src/test/java/com/example/crosstheroad/NonNullDifficultyTest.java
@@ -0,0 +1,19 @@
+package com.example.crosstheroad;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import android.widget.RadioButton;
+
+/**
+ * Class checks when a non-null button is clicked for choosing a difficulty
+ */
+public class NonNullDifficultyTest {
+    RadioButton rb1 = new RadioButton(null);
+    @Test
+    public void nonNullButton() {
+        rb1.setChecked(true);
+        assertTrue(Configuration.difficultyLevel(rb1));
+    }
+}
