88a4c31ec122a0642a0034746fce870bcfbdf312
Shriya Jitta <sjitta6@gatech.edu>
2023-03-14 21:30:53 -0400

    "added test10"

---
+++ CrossTheRoad/app/src/test/java/com/example/crosstheroad/Sprint3Test10.java
@@ -0,0 +1,26 @@
+package com.example.crosstheroad;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import android.graphics.Canvas;
+
+import org.junit.Test;
+
+public class Sprint3Test10 {
+    /***
+     * current row 8 and highest row 15 so char on safe tile, checks if expected score equals the actual score
+     */
+
+    @Test
+    public void testUpdateScore() {
+        GameActivity.currentRow = 8;
+        GameActivity.highestRow = 15;
+        int expectedScore = 0;
+
+        int actualScore = GameActivity.updateScore();
+
+        assertEquals(expectedScore, actualScore);
+    }
+}
