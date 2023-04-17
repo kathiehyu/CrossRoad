5e7c907fcc5c7fa0eb36b05afe5f7ed1124d6b68
Diep, Xuan Vui <xdiep3@gatech.edu>
2023-04-16 23:30:54 -0400

    unit tests

---
+++ CrossTheRoad/app/src/test/java/com/example/crosstheroad/Sprint5TestJumpOutLogs.java
@@ -0,0 +1,18 @@
+package com.example.crosstheroad;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+public class Sprint5TestJumpOutLogs {
+    /**
+     * this test check if the character collide with the first obstacle
+     */
+    @Test
+    public void testFirstObstacleColliding(){
+        int charX = 0;
+        int obstacleX = 0;
+        assertTrue(GameActivity.checkObstacleColliding(charX, obstacleX));
+    }
+
+}
